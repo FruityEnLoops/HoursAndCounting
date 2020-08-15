a305bb0141fb6fe6a7b17e13cf8d870eb1fb80c4
FruityEnLoops <fruityenloops@gmail.com>
2020-08-16 01:02:14 +0200

    Parser can now parse iCal raw data

--- src/main/java/Main/CalendarEvent.java
+++ src/main/java/Main/CalendarEvent.java
@@ -3,6 +3,8 @@ package Main;
 import java.time.LocalDateTime;
 
 public class CalendarEvent {
+    private final String UNDEFINED_DEFAULT = "undefined";
+
     String category;
     String uid;
     LocalDateTime start;
@@ -11,11 +13,49 @@ public class CalendarEvent {
     String location;
     String description;
 
+    public CalendarEvent() {
+        this.category = UNDEFINED_DEFAULT;
+        this.uid = UNDEFINED_DEFAULT;
+        this.start = null;
+        this.end = null;
+        this.summary = UNDEFINED_DEFAULT;
+        this.location = UNDEFINED_DEFAULT;
+        this.description = UNDEFINED_DEFAULT;
+    }
+
     public String toString(){
         return category +
-                "\nDe " + start.toString() +
-                " a " + end.toString() +
+                "\nDe " + start +
+                " a " + end +
                 "\n" + summary +
                 "\nDans : " + location;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
--- src/main/java/Parser/Parser.java
+++ src/main/java/Parser/Parser.java
@@ -6,9 +6,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Parser {
+    private static final String SUMMARY = "SUMMARY:";
+    private static final String UID = "UID:";
+    private static final String DTSTART = "DTSTART;";
+    private static final String DTEND = "DTEND;";
+    private static final String LOCATION = "LOCATION:";
+    private static final String DESCRIPTION = "DESCRIPTION:";
+    private static final String CATEGORY = "CATEGORY:";
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.BASIC_ISO_DATE;
+
+
     // opens the stream, and returns the whole file.
     public static String rawDataParser(String url) throws IOException {
         String buffer;
@@ -21,7 +37,75 @@ public class Parser {
     }
 
     public static ArrayList<CalendarEvent> parseRawData(String rawData){
-        // TODO : parse raw data function
-        return null;
+        ArrayList<String> rawEvents = getRawEventsList(rawData);
+        ArrayList<CalendarEvent> res = new ArrayList<>();
+        for(String e : rawEvents){
+            CalendarEvent event = new CalendarEvent();
+            Scanner sc = new Scanner(e);
+            while(sc.hasNext()){
+                String line = sc.nextLine();
+                // Detect what that line is about, to add it to the right field in our event object
+                if(line.startsWith(SUMMARY)){
+                    event.setSummary(line.substring(SUMMARY.length()));
+                } else if(line.startsWith(UID)){
+                    event.setUid(line.substring(UID.length()));
+                } else if(line.startsWith(DTSTART)){
+                    String date = line.substring(line.indexOf(":") + 1);
+                    event.setStart(parseDate(date));
+                } else if(line.startsWith(DTEND)){
+                    String date = line.substring(line.indexOf(":") + 1);
+                    event.setEnd(parseDate(date));
+                } else if(line.startsWith(LOCATION)){
+                    event.setLocation(line.substring(LOCATION.length()));
+                } else if(line.startsWith(DESCRIPTION)){
+                    event.setDescription(line.substring(DESCRIPTION.length()));
+                } else if(line.startsWith(CATEGORY)){
+                    event.setCategory(line.substring(CATEGORY.length()));
+                }
+            }
+            // don't forget to close the scanner after we're done with this object!
+            sc.close();
+            res.add(event);
+        }
+        return res;
+    }
+
+    private static LocalDateTime parseDate(String line) {
+        Scanner sc = new Scanner(line);
+        sc.useDelimiter("T");
+        LocalDate date = LocalDate.parse(sc.next(), formatter);
+        LocalTime time;
+        if(sc.hasNext()){
+            time = LocalTime.parse(sc.next(), DateTimeFormatter.ofPattern("HHmmss")).plusHours(2);
+        } else {
+            time = LocalTime.of(0, 0);
+        }
+        return LocalDateTime.of(date, time);
+    }
+
+    public static ArrayList<String> getRawEventsList(String rawData) {
+        Scanner sc = new Scanner(rawData);
+        ArrayList<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        sc.useDelimiter("\n");
+        // skip to the first VEVENT
+        boolean done = false;
+        while(!done && sc.hasNext()){
+            if(sc.nextLine().equals("BEGIN:VEVENT")){
+                done = true;
+            }
+        }
+        // add each VEVENT to the ArrayList
+        while(sc.hasNext()){
+            String line = sc.nextLine();
+            sb.append(line);
+            if(line.equals("END:VEVENT")){
+                res.add(sb.toString());
+                sb = new StringBuilder();
+            }
+            sb.append("\n");
+        }
+        sc.close();
+        return res;
     }
 }
